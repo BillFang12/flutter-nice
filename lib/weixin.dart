d79f90281e354bcdcb3828af60ce20e9f2f419d3
BillFang12 <fzx_ka@126.com>
2019-04-21 14:26:51 +0800

    xx

--- lib/weixin.dart
+++ lib/weixin.dart
@@ -8,10 +8,25 @@ class WeixinPage extends StatelessWidget{
       appBar: AppBar(
         title: Text("微信"),
         centerTitle: true,
+        actions: <Widget>[
+          Image.asset("img/add_img.png",)
+        ],
       ),
       body: Column(
         children: <Widget>[
-         
+          Row(
+            children: <Widget>[
+                Image.asset("img/person.png"),
+                Expanded(
+                  child: Text("你的第一个好友"),
+                )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+               
+            ],
+          )
         ],
       ),
     );
